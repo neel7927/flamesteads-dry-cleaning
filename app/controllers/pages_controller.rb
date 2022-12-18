0e1e12952ef0797df6829b2f278e0f1bc107e291
Mubeen Bankur <mobee33@hotmail.com>
2022-12-18 17:34:18 +0400

    launching page completed

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,10 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :home ]
-
+  skip_before_action :authenticate_user!, only: [ :launchpage ]
   def home
+  end
+
+  def launchpage
+    redirect_to home_path if signed_in?
+    @skip_navbar = true
   end
 end
